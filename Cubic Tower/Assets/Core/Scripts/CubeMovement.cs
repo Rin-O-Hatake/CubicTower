c0d8a17722051ec3f540f1c012d4aeea134760a1
Rin-O-Hatake <89845456+Rin-O-Hatake@users.noreply.github.com>
2025-03-10 15:36:37 +0300

    Add Project

---
+++ Cubic Tower/Assets/Core/Scripts/CubeMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.EventSystems;
+
+namespace Core.Scripts
+{
+    public class CubeMovement : IDragHandler
+    {
+        public void OnDrag(PointerEventData eventData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
